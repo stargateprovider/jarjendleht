0413590b222c6541ce6ac6f813df584d9d2fe22f
stargateprovider <kana46@windowslive.com>
2019-11-23 21:32:04 +0200

    Lisa kontekstmenüü

--- masin.js
+++ masin.js
@@ -4,3 +4,22 @@ $(function() {
 		snap: true
 	});
 });
+
+document.addEventListener("DOMContentLoaded", e=>{
+	if (document.addEventListener) {
+		document.addEventListener('contextmenu', function(e) {
+			e.preventDefault();
+			let x = event.clientX;
+			let y = event.clientY;
+			contextmenu = document.getElementById("menu")
+			contextmenu.style.display = "block";
+			contextmenu.style.left = x.toString() + "px";
+			contextmenu.style.top = y.toString() + "px";
+		}, false);
+	} else {
+		document.attachEvent('oncontextmenu', function() {
+			alert("You've tried to open context menu");
+			window.event.returnValue = false;
+		});
+	}
+});
