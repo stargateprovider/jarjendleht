eae0690bcc9bff8cc75f3713f2cafe2e30d8108f
stargateprovider <kana46@windowslive.com>
2019-11-25 19:22:03 +0200

    Tegin elementide ühendamise loogika

--- masin.js
+++ masin.js
@@ -1,3 +1,5 @@
+var sisendid = {};
+
 $(function() {
 	$(".loogikaelement").draggable({
 		grid: [32, 32],
@@ -7,61 +9,77 @@ $(function() {
 
 
 // Main funktsioon
-document.addEventListener("DOMContentLoaded", function(){
-	document.addEventListener('contextmenu', function(e) {
-		e.preventDefault();
+window.addEventListener("load", function(){
+	document.getElementById("canvas").addEventListener('contextmenu', e=>{
+		// Teeb kontekstmenüü
 		let x = event.clientX;
 		let y = event.clientY;
-		contextmenu = document.getElementById("menu")
+		var esimene = document.elementFromPoint(x, y);
+
+		e.preventDefault();
+		var contextmenu = document.getElementById("menu");
 		contextmenu.style.display = "block";
 		contextmenu.style.left = x.toString() + "px";
 		contextmenu.style.top = y.toString() + "px";
 
+		var uhenda = function(event){
+			// Alustab elementide ühendamise
+			contextmenu.style.display = "none";
+
+			var uhenda2 = function(event){
+				// Ühendab valitud elemendid
+				teine = event.target;
+				let onLoogikaElement = teine.className.includes("loogikaelement") || teine.className.includes("sisendelement");
+				let poleElementIse = esimene != teine;
+
+				if (onLoogikaElement && poleElementIse){
+					teine.style.boxShadow="0px 0px 20px #880000";
+					sisendid[esimene.getAttribute("data-id")].add(teine.getAttribute("data-id"));
+					this.removeEventListener("click", uhenda2);
+				}
+			}
+			this.removeEventListener("click", uhenda);
+			document.addEventListener("click", uhenda2);
+		};
+		document.getElementById("btn-input").addEventListener("click", uhenda);
 	}, false);
 
+
+	sisendid[document.getElementsByClassName("sisendelement")[0].getAttribute("data-id")] = new Set();
+	sisendid[document.getElementsByClassName("valjundelement")[0].getAttribute("data-id")] = new Set();
+
+	//document.getElementById("kaivita").onclick = leiaValjund;
 	var a=document.getElementById("klaveritegija");
-	a.onclick=mutus;
-	b=document.getElementById("emme");
-	b.onclick=mutus1;
-	nupp_uhendus=document.getElementById("btn-input");
-	nupp_uhendus.onclick=uhenda;
+	a.onclick=lisaSisend;
+
+	var emmed=document.getElementsByClassName("emme");
+	for (var i=0; i<emmed.length; i++){
+		emmed[i].addEventListener("click", e=>{
+			lisaElement(e.target.parentNode.getAttribute('data-id'));
+		});
+	}
 });
 
-function mutus(){
-	var a=document.getElementById("sisendid");
-	/*a.innerHTML+="<br><input type=\"text\" name=\"sisend\" placeholder=\"bitijada\">";*/
-	a.appendChild(document.createElement("br"))
-	input = a.appendChild(document.createElement("input"))
-	input.type = "text"
-	input.name = "sisend"
-	input.placeholder = "bitijada"
+function lisaElement(type, cls="loogikaelement"){
+	var canvas = document.getElementById("canvas");
+	var img = canvas.appendChild(document.createElement("img"));
+	img.className = cls;
+	img.src="elemendid/" + type + ".png";
+	index = canvas.getElementsByClassName(cls).length.toString();
+	img.setAttribute("data-id", type+index);
+	$(function(){$(img).draggable({grid:[32,32], snap:true});});
+	$(img).css({top:50, left:100});
+
+	sisendid[img.getAttribute("data-id")] = new Set();
 }
-function mutus1(){
-	var a=document.getElementById("side");
-	div = a.appendChild(document.createElement("div"));
-	div.className="loogikaelement ui-draggable ui-draggable-handle";
-	im=div.appendChild(document.createElement("img"));
-	im.src="elemendid/or.png"
-	$(function(){$(div).draggable({grid:[32,32],snap:true});});
-	$(div).css({top:200,left:200,position:"absolute"});
 
-}
-function uhenda(){
-	contextmenu = document.getElementById("menu")
-	contextmenu.style.display = "none";
-	var helendus=function(event){
-		esimene=event.target;
-		esimene.style.boxShadow="0px 0px 20px #880000";
-		this.removeEventListener("click",helendus);
-		return esimene;
-	}
-	document.addEventListener("click",helendus);
-
-
-	nupp_uhendus=document.getElementById("btn-input");
-	nupp_uhendus.style.boxShadow="";
-
-}
-function connekt(a,b){
-	alert("a,b");
+function lisaSisend(){
+	let ol = document.getElementById("sisendid");
+	let li = ol.appendChild(document.createElement("li"));
+	let input = li.appendChild(document.createElement("input"));
+	index = ol.children.length.toString();
+	input.name = "sisend"+index;
+	input.placeholder = "bitijada";
+	input.type = "text";
+	lisaElement("sisend", "sisendelement");
 }
