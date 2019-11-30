64c467fcb7cca04ac7f3d81d5b3998d7408568a3
kiistata <kariniistata@gmail.com>
2019-12-01 01:40:41 +0200

    muutuva taustaga kohati

---
+++ tutorial.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function(){
+  document.getElementById("nupp").addEventListener("click",function(){
+    document.getElementsByTagName("body")[0].style.backgroundColor="blue";
+  });
+});
