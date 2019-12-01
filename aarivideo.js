eb18d3769ffb36eb72a1861a7df71b5921a9fd09
kiistata <kariniistata@gmail.com>
2019-12-01 02:10:09 +0200

    maini jssi tegemine

--- aarivideo.js
+++ aarivideo.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){
-  document.getElementById("nupp").addEventListener("click",function(){
+  document.getElementById("nupp1").addEventListener("click",function(){
     if (document.getElementById("sisend").value=="Toomast"){
       document.getElementById("kusimus").innerHTML="Õige";
     };
--- tutorial.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function(){
-  document.getElementById("nupp").addEventListener("click",function(){
-    document.getElementsByTagName("body")[0].style.backgroundColor="blue";
-  });
-});
